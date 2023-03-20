1b6104d03f625c1dfb235cd830e5b7019a6963a3
Lucas Wenck <lucas.wenck@gmail.com>
2023-03-20 20:06:27 -0300

    Create conditional that updates buy and sell day

--- stock-picker.rb
+++ stock-picker.rb
@@ -6,4 +6,15 @@ def stock_picker (prices)
     (0...prices.length).each do |buy_day|
         ((buy_day + 1)...prices.length).each do |sell_day|
             profit = prices[sell_day] - prices[buy_day]
+            if profit > max_profit
+                max_profit = profit
+                best_buy_day = buy_day
+                best_sell_day = sell_day
+            end
+        end
+    end
+
+    p [best_buy_day, best_sell_day]
 end
+
+stock_picker([17,3,6,9,15,8,6,1,10])
