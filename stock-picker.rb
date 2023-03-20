cb7f338f7285bfa3a7127be57bfa4b05683db0c9
Lucas Wenck <lucas.wenck@gmail.com>
2023-03-20 20:02:55 -0300

    Create nested loops to iterate over every pair

--- stock-picker.rb
+++ stock-picker.rb
@@ -3,5 +3,7 @@ def stock_picker (prices)
     best_sell_day = 0
     max_profit = 0
 
-    
+    (0...prices.length).each do |buy_day|
+        ((buy_day + 1)...prices.length).each do |sell_day|
+            profit = prices[sell_day] - prices[buy_day]
 end
